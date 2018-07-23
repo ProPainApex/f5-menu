6e2ddfc28b5a412071f44d1820c347b2e58c3d16
Calle <36276161+qalleh1@users.noreply.github.com>
2018-07-23 23:03:37 +0200

    added

---
+++ README.md
@@ -0,0 +1,17 @@
+# f5_menu
+
+[REQUIREMENTS]
+  
+* ESX Support
+  * https://github.com/jonassvensson4/jsfour-toilet
+  * https://github.com/jonassvensson4/jsfour-blindfold
+  * https://github.com/jonassvensson4/jsfour-legitimation
+
+[INSTALLATION]
+
+1) CD in your resources/[esx] folder
+
+2) Add this in your server.cfg :
+``start f5-menu``
+
+lua
---
+++ client.lua
@@ -0,0 +1,96 @@
+local Keys = {
+  ["ESC"] = 322, ["F1"] = 288, ["F2"] = 289, ["F3"] = 170, ["F5"] = 166, ["F6"] = 167, ["F7"] = 168, ["F8"] = 169, ["F9"] = 56, ["F10"] = 57,
+  ["~"] = 243, ["1"] = 157, ["2"] = 158, ["3"] = 160, ["4"] = 164, ["5"] = 165, ["6"] = 159, ["7"] = 161, ["8"] = 162, ["9"] = 163, ["-"] = 84, ["="] = 83, ["BACKSPACE"] = 177,
+  ["TAB"] = 37, ["Q"] = 44, ["W"] = 32, ["E"] = 38, ["R"] = 45, ["T"] = 245, ["Y"] = 246, ["U"] = 303, ["P"] = 199, ["["] = 39, ["]"] = 40, ["ENTER"] = 18,
+  ["CAPS"] = 137, ["A"] = 34, ["S"] = 8, ["D"] = 9, ["F"] = 23, ["G"] = 47, ["H"] = 74, ["K"] = 311, ["L"] = 182,
+  ["LEFTSHIFT"] = 21, ["Z"] = 20, ["X"] = 73, ["C"] = 26, ["V"] = 0, ["B"] = 29, ["N"] = 249, ["M"] = 244, [","] = 82, ["."] = 81,
+  ["LEFTCTRL"] = 36, ["LEFTALT"] = 19, ["SPACE"] = 22, ["RIGHTCTRL"] = 70,
+  ["HOME"] = 213, ["PAGEUP"] = 10, ["PAGEDOWN"] = 11, ["DELETE"] = 178,
+  ["LEFT"] = 174, ["RIGHT"] = 175, ["TOP"] = 27, ["DOWN"] = 173,
+  ["NENTER"] = 201, ["N4"] = 108, ["N5"] = 60, ["N6"] = 107, ["N+"] = 96, ["N-"] = 97, ["N7"] = 117, ["N8"] = 61, ["N9"] = 118
+}
+
+ESX              = nil
+local PlayerData = {}
+
+Citizen.CreateThread(function()
+	while ESX == nil do
+		TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+		Citizen.Wait(0)
+	end
+end)
+
+RegisterNetEvent('esx:playerLoaded')
+AddEventHandler('esx:playerLoaded', function(xPlayer)
+  PlayerData = xPlayer   
+end)
+
+RegisterNetEvent('esx:setJob')
+AddEventHandler('esx:setJob', function(job)
+  PlayerData.job = job
+end)
+
+function openMenu()
+    ESX.UI.Menu.CloseAll()
+
+    ESX.UI.Menu.Open(
+        'default', GetCurrentResourceName(), 'person_menu',
+        {
+            title    = 'Meny',
+            elements = {
+                {label = 'ID-Kort', value = 'id-card'},
+                {label = 'Blindfold', value = 'blindfold'},
+                {label = 'Kissa', value = 'pee'},
+                {label = 'Bajsa', value = 'poop'},
+            }
+        },
+        function(data, menu)
+            if data.current.value == 'id-card' then
+                ESX.UI.Menu.Open(
+                    'default', GetCurrentResourceName(), 'id_card_menu',
+                    {
+                        title    = 'ID meny',
+                        elements = {
+                            {label = 'Kolla Körkort', value = 'check'},
+                            {label = 'Visa Körkort för närmaste person', value = 'show'}
+                        }
+                    },
+                    function(data2, menu2)
+                        if data2.current.value == 'check' then
+                            TriggerServerEvent('jsfour-legitimation:open', GetPlayerServerId(PlayerId()), GetPlayerServerId(PlayerId()))
+                        elseif data2.current.value == 'show' then
+                            local player, distance = ESX.Game.GetClosestPlayer()
+
+                            if distance ~= -1 and distance <= 3.0 then
+                                TriggerServerEvent('jsfour-legitimation:open', GetPlayerServerId(PlayerId()), GetPlayerServerId(player))
+                            else
+                                ESX.ShowNotification('Ingen i närheten')
+                            end
+                        end
+                    end,
+                    function(data2, menu2)
+                        menu2.close()
+                    end
+                )
+            elseif data.current.value == 'pee' then
+                 TriggerEvent('pee')
+            elseif data.current.value == 'poop' then
+                 TriggerEvent('poop')
+            elseif data.current.value == 'blindfold' then
+                    local player, distance = ESX.Game.GetClosestPlayer()
+
+                    if distance ~= -1 and distance <= 3.0 then
+                       ESX.TriggerServerCallback('jsfour-blindfold:itemCheck', function( hasItem )
+                          TriggerServerEvent('jsfour-blindfold', GetPlayerServerId(player), hasItem)
+                        end)
+                    else
+                       ESX.ShowNotification('Ingen i närheten')        
+                    end    
+
+            end
+        end,
+        function(data, menu)
+            menu.close()
+        end
+    )
+end
